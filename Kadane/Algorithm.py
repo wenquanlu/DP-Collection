22d90f43c1344e98f9575625422b9bf3e1ba63c1
welu3614 <welu3614@uni.sydney.edu.au>
2021-05-05 21:56:22 +1000

    changed

---
+++ Kadane/Algorithm.py
@@ -0,0 +1,12 @@
+import sys
+"""
+local_max = max(A[i], A[i] + local_max)
+"""
+def max_sum_subarray(array):
+    local_max = 0
+    global_max = -sys.maxsize - 1
+    for i in range(len(array)):
+        local_max = max(array[i], array[i] + local_max)
+        if (local_max > global_max):
+            global_max = local_max
+    return global_max
---
+++ Kadane/Demo.py
@@ -0,0 +1,7 @@
+from Algorithm import max_sum_subarray
+
+arr = [1, 2, 4, 1, 0, -1, -10, 20, 21, -30, 0, -1, -100, 22, 3, 4, 56, -2, 0, 23]
+
+x = max_sum_subarray(arr)
+
+print(x)   #106 = 22 + 3 + 4 + 56 + (-2) + 0 + 23
